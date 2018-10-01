7dbd802af65f0dd8e0e2528c18fdb9dba73c8635
Nathaniel Smith <ns381@uowmail.edu.au>
2018-10-01 19:43:37 +1000

    Error Handling + Bug Fixes
    Error handling has been added to all methods
    
    Bug Fixes
    + Image lists now update between milestones
    + server no longer crashes when entering incorrect values in search milestone
    + .DS_Store handling has been added and are now ignored

--- app.js
+++ app.js
@@ -56,90 +56,122 @@ var milestoneResults;
 
 
 app.get("/", (req, res) => {
-        res.sendFile(workingDir + "/html/index.html");
-
-        });
-
-
+    res.sendFile(workingDir + "/html/index.html");
+});
 
 app.get("/add", (req, res) => {
-
-        res.sendFile(workingDir + "/html/add.html");
-        });
-
-
+    res.sendFile(workingDir + "/html/add.html");
+});
 
 app.get("/addConfirmed", (req, res) => {
-        res.sendFile(workingDir + "/html/addConfirmed.html");
-        });
+    res.sendFile(workingDir + "/html/addConfirmed.html");
+});
 
+app.get("/addFail", (req, res) => {
+    res.sendFile(workingDir + "/html/addFail.html");
+});
 
+app.get("/userExists", (req, res) => {
+    res.sendFile(workingDir + "/html/userExists.html");
+});
 
 app.get("/update", (req, res) => {
-        res.sendFile(workingDir + "/html/update.html");
-        });
+    res.sendFile(workingDir + "/html/update.html");
+});
 
 app.get("/updateConfirmed", (req, res) => {
-        res.sendFile(workingDir + "/html/updateConfirmed.html");
-        });
+    res.sendFile(workingDir + "/html/updateConfirmed.html");
+});
 
 app.get("/updateFail", (req, res) => {
-        res.sendFile(workingDir + "/html/updateFail.html");
-        });
+    res.sendFile(workingDir + "/html/updateFail.html");
+});
 
 app.get("/search", (req, res) => {
-        res.sendFile(workingDir + "/html/search.html");
-        });
+    res.sendFile(workingDir + "/html/search.html");
+});
 
 app.get("/searchConfirmed", (req, res) => {
     var searchData = {result:searchResults};
     var page = fs.readFileSync(workingDir + "/html/searchConfirmed.html", "utf8");
     var html = mustache.to_html(page, searchData);
     res.send(html);;
-        });
+});
 
 app.get("/searchFail", (req, res) => {
-        res.sendFile(workingDir + "/html/searchFail.html");
-      });
+    res.sendFile(workingDir + "/html/searchFail.html");
+});
 
 app.get("/remove", (req, res) => {
-        res.sendFile(workingDir + "/html/remove.html");
-        });
+    res.sendFile(workingDir + "/html/remove.html");
+});
 
 app.get("/removeConfirmed", (req, res) => {
-        res.sendFile(workingDir + "/html/removeConfirmed.html");
-        });
+    res.sendFile(workingDir + "/html/removeConfirmed.html");
+});
 
 app.get("/removeFail", (req, res) => {
-        res.sendFile(workingDir + "/html/removeFail.html");
-        });
+    res.sendFile(workingDir + "/html/removeFail.html");
+});
 
 app.get("/milestones", (req, res) => {
-        res.sendFile(workingDir + "/html/milestones.html");
-        });
+    res.sendFile(workingDir + "/html/milestones.html");
+});
 
 app.get("/milestoneConfirmed", (req, res) => {
     var searchData = {result:milestoneResults};
     var page = fs.readFileSync(workingDir + "/html/milestoneConfirmed.html", "utf8");
     var html = mustache.to_html(page, searchData);
     res.send(html);
-        });
+});
+
+app.get("/milestoneSearchFail", (req, res) => {
+    res.sendFile(workingDir + "/html/milestoneSearchFail.html");
+});
 
 app.get("/displayPics", (req, res) => {
     var imgData = {pictures:picObject};
     var page = fs.readFileSync(workingDir + "/html/viewPics.html", "utf8");
     var html = mustache.to_html(page, picObject);
     res.send(html);
-        });
+});
 
 //_____________POST METHODS__________//
 
 
 app.post("/addname", (req, res) => {
     var userToAdd = new users(req.body);
-    userToAdd.save(); // Need to check is user already here?
-    res.redirect("/addConfirmed");
-         });
+    var p = req.body.password;
+    var pConf = req.body.passwordConf;
+    var searchString = req.body.email;
+    var query = users.findOne({});
+    if (p == pConf )
+    {
+      query.where('email').in([searchString]);
+
+      query.exec(function (err, result)
+      {
+        if (result == null)
+        {
+          userToAdd.save();
+          res.redirect("/addConfirmed");
+        }
+        else
+        {
+
+          res.redirect("/userExists")
+        }
+
+        });
+    }
+
+    else
+    {
+      res.redirect("/addFail");
+    }
+
+
+ });
 
 app.post("/search", (req, res) => { //searches users
     var searchString = req.body.data;
@@ -211,31 +243,66 @@ app.post("/update", (req, res) => { //Updates users password
 });
 
 app.post("/searchMilestone", (req, res) => {
-    var searchString = req.body.data;
-    var query = milestone.find({});
-    query.where('email').in([searchString]);
+  var searchString = req.body.data;
+  var query = users.findOne({});
+  query.where('email').in([searchString]);
 
-    query.exec(function (err, result)
+  query.exec(function (err, result)
+  {
+
+    if (result == null)
     {
-        milestoneResults = result;
-        res.redirect("/milestoneConfirmed")
+      res.redirect("/milestoneSearchFail");
+    }
+    else
+    {
+      var mquery = milestone.find({});
+      mquery.where('email').in([searchString]);
+
+      mquery.exec(function (err, result)
+      {
+          if (result.length == 0)
+          {
+            res.redirect("/milestoneSearchFail");
+          }
+          else
+          {
+          milestoneResults = result;
+          res.redirect("/milestoneConfirmed");
+          }
+      });
+    }
+
     });
 
  });
 
 app.post("/viewPics", (req, res) => {
+    tempPicObject = new infoContainer;
     var picDir = "/Images/" + req.body.username + "/" + req.body.milestonename + "/";
-
+    var objectIndex = 0;
     fs.readdir(workingDir + picDir, function(err, items)
     {
+    // IF ITEMS == NULL OR UNDEFINED DO SOMETHING (ERROR HANDLING)
       for (var i=0; i<items.length; i++)
       {
-        var picInfo = new info;
-        picInfo.name = items[i];
-        picInfo.dir = picDir + items[i];
-        picObject.info[i] = picInfo;
+        if (items[i] == ".DS_Store")
+        {
+          console.log("Found a DS_Store, Ignoring it");
+        }
+        else
+        {
+          var picInfo = new info;
+          picInfo.name = items[i];
+          picInfo.dir = picDir + items[i];
+          tempPicObject.info[objectIndex] = picInfo;
+          objectIndex++;
+        }
+
       }
     });
+         objectIndex = 0;
+         picObject = tempPicObject;
          res.redirect("/displayPics");
  });
 
